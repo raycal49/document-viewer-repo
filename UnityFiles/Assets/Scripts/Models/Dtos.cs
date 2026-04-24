a6f862e858900282eb34fd4b9828ac872b778621
Raymond Calderon <rcalder7@CougarNet.UH.EDU>
2026-04-24 10:45:14 -0500

    Implement US-04 DTOs and add Unity editor debug harness

--- UnityFiles/Assets/Scripts/Models/Dtos.cs
+++ UnityFiles/Assets/Scripts/Models/Dtos.cs
@@ -6,15 +6,52 @@ using UnityEngine;
 [Serializable] public class IceServerData { public string[] urls; public string username; public string credential; }
 [Serializable] public class AnnotationPoint { public float x; public float y; }
 [Serializable] public class AnnotationData {
-  public string Type {get; set;} 
-  public float[][] Vector {get; set;} 
-  public int[] Color {get;set;} 
-  public string StrokeType {get;set;} 
-  public float FadeDuration {get;set;} 
+  public string Type {get; set;}
+  public float[][] Vector {get; set;}
+  public int[] Color {get;set;}
+  public string StrokeType {get;set;}
+  public float FadeDuration {get;set;}
   }
-[Serializable] public class ParsedAnnotation { 
-  public AnnotationPoint[] points; 
+[Serializable] public class ParsedAnnotation {
+  public AnnotationPoint[] points;
   public string color;
   public bool isFading;
-  public float FadeDurationSeconds; 
+  public float FadeDurationSeconds;
   }
+
+[Serializable] public class DocumentStartMessage
+{
+    public string documentId;
+    public string documentName;
+    public int totalPages;
+}
+
+[Serializable] public class DocumentPageMessage
+{
+    public string documentId;
+    public int pageIndex;
+    public int totalPages;
+    public int width;
+    public int height;
+    public int chunkIndex;
+    public int totalChunks;
+    public byte[] data;
+}
+
+[Serializable] public class DocumentCloseMessage
+{
+    public string documentId;
+}
+
+[Serializable] public class DocumentRequestPageMessage
+{
+    public string documentId;
+    public int pageIndex;
+}
+
+[Serializable] public class DocumentNavigateMessage
+{
+    public string documentId;
+    public int pageIndex;
+    public string source;
+}
